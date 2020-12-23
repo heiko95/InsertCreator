7549652ee150c1cd4ef9730207fbc0ef6d24c938
Gruber Heiko <Heiko.Gruber@hoefliger.de>
2020-12-23 11:30:18 +0100

    Add Model

---
+++ Liedeinblendung/Liedeinblendung/Model/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liedeinblendung.Model
+{
+    public class Song
+    {
+        public string Number { get; set; }
+
+        public string Title { get; set; }
+
+        public int Versecount { get; set; }
+
+        public List<Verse> Verses { get; set; } = new List<Verse>();
+
+        public List<Metadata> Metadatas { get; set; } = new List<Metadata>();
+    }
+}
