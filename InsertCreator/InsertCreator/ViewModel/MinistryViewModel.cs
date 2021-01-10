8fbb8a9dc46c0265f44b40b13e3edde91e536775
heiko95 <39989555+heiko95@users.noreply.github.com>
2021-01-10 19:03:54 +0100

    Ministry

--- InsertCreator/InsertCreator/ViewModel/MinistryViewModel.cs
+++ InsertCreator/InsertCreator/ViewModel/MinistryViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +16,13 @@ namespace Liedeinblendung.ViewModel
         {
             Ministries.Add(new Ministry(){Function = "Di", ForeName =  "Horst", SureName = "Häußermann" });
             Ministries.Add(new Ministry() { Function = "U.Di", ForeName = "Mischa", SureName = "Häußermann" });
+            MinistrieNames.Add("Horst Peterson");
+            MinistrieNames.Add("ff sgsd");
+
         }
 
         public ObservableCollection<Ministry> Ministries { get; set; } = new ObservableCollection<Ministry>();
 
+        public ObservableCollection<string> MinistrieNames { get; set; } = new ObservableCollection<string>();
     }
 }
