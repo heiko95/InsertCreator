e3ebf3d47e72967efe9c8264f09237aee9d51064
heiko95 <39989555+heiko95@users.noreply.github.com>
2021-01-18 11:59:41 +0100

    Add BibleInsert

---
+++ InsertCreator/InsertCreator/Model/BibleChapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HgSoftware.InsertCreator.Model
+{
+    public class BibleChapter
+    {
+        public int Number { get; set; }
+        public int Versecount { get; set; }
+    }
+}
