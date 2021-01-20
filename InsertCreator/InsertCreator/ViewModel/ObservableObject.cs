f74a837a6ffd26980cfb27895cb6c5e7d7551788
Gruber Heiko <Heiko.Gruber@hoefliger.de>
2021-01-20 15:52:41 +0100

    Validation

--- InsertCreator/InsertCreator/ViewModel/ObservableObject.cs
+++ InsertCreator/InsertCreator/ViewModel/ObservableObject.cs
@@ -95,42 +95,6 @@ namespace HgSoftware.InsertCreator.ViewModel
 
         #endregion Protected Methods
 
-        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _propertyErrors.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName) 
-        {
-            // return _propertyErrors.GetValueOrDefault(propertyName, null);
-
-            return _propertyErrors.TryGetValue(propertyName, out var value) ? value : default(List<string>);
-        }
-
-        public void AddError(string propertyName, string errorMessage)
-        {
-            if (!_propertyErrors.ContainsKey(propertyName))
-            {
-                _propertyErrors.Add(propertyName, new List<string>());
-            }
-
-            _propertyErrors[propertyName].Add(errorMessage);
-            OnErrorsChanged(propertyName);
-        }
-
-        public void ClearErrors(string propertyName)
-        {
-            if (_propertyErrors.Remove(propertyName))
-            {
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
+       
     }
 }
