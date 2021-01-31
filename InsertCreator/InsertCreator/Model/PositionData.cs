4cf2fa35b1d1858da95f191ca2cb769165723906
BuildTools <unconfigured@null.spigotmc.org>
2021-01-31 19:54:39 +0100

    Add position data properties

--- InsertCreator/InsertCreator/Model/PositionData.cs
+++ InsertCreator/InsertCreator/Model/PositionData.cs
@@ -32,10 +32,10 @@ namespace HgSoftware.InsertCreator.Model
         private PointF _leftTextTwoRowSecondLine = new PointF();
 
         [JsonProperty]
-        private PointF _logoPositionLeft = new PointF();
+        private PointF _leftLogoPosition = new PointF();
 
         [JsonProperty]
-        private PointF _logoPositionRight = new PointF();
+        private PointF _rightLogoPosition = new PointF();
 
         [JsonProperty]
         private PointF _rightTextFourRowFirstLine = new PointF();
@@ -63,17 +63,8 @@ namespace HgSoftware.InsertCreator.Model
             _cornerbugPosition.X = 1;
             _cornerbugPosition.Y = 1;
 
-            _leftTextFourRowFirstLine.X = 1;
-            _leftTextFourRowFirstLine.Y = 1;
-
-            _leftTextFourRowFourthLine.X = 1;
-            _leftTextFourRowFourthLine.Y = 1;
-
-            _leftTextFourRowSecondLine.X = 1;
-            _leftTextFourRowSecondLine.Y = 1;
-
-            _leftTextFourRowThirdLine.X = 1;
-            _leftTextFourRowThirdLine.Y = 1;
+            _leftLogoPosition.X = 1;
+            _leftLogoPosition.Y = 1;
 
             _leftTextOneRowFirstLine.X = 1;
             _leftTextOneRowFirstLine.Y = 1;
@@ -84,23 +75,20 @@ namespace HgSoftware.InsertCreator.Model
             _leftTextTwoRowSecondLine.X = 1;
             _leftTextTwoRowSecondLine.Y = 1;
 
-            _logoPositionLeft.X = 1;
-            _logoPositionLeft.Y = 1;
+            _leftTextFourRowFirstLine.X = 1;
+            _leftTextFourRowFirstLine.Y = 1;
 
-            _logoPositionRight.X = 1;
-            _logoPositionRight.Y = 1;
+            _leftTextFourRowSecondLine.X = 1;
+            _leftTextFourRowSecondLine.Y = 1;
+            
+            _leftTextFourRowThirdLine.X = 1;
+            _leftTextFourRowThirdLine.Y = 1;
 
-            _rightTextFourRowFirstLine.X = 1;
-            _rightTextFourRowFirstLine.Y = 1;
+            _leftTextFourRowFourthLine.X = 1;
+            _leftTextFourRowFourthLine.Y = 1;
 
-            _rightTextFourRowFourthLine.X = 1;
-            _rightTextFourRowFourthLine.Y = 1;
-
-            _rightTextFourRowSecondLine.X = 1;
-            _rightTextFourRowSecondLine.Y = 1;
-
-            _rightTextFourRowThirdLine.X = 1;
-            _rightTextFourRowThirdLine.Y = 1;
+            _rightLogoPosition.X = 1;
+            _rightLogoPosition.Y = 1;
 
             _rightTextOneRowFirstLine.X = 1;
             _rightTextOneRowFirstLine.Y = 1;
@@ -110,6 +98,18 @@ namespace HgSoftware.InsertCreator.Model
 
             _rightTextTwoRowSecondLine.X = 1;
             _rightTextTwoRowSecondLine.Y = 1;
+
+            _rightTextFourRowFirstLine.X = 1;
+            _rightTextFourRowFirstLine.Y = 1;
+
+            _rightTextFourRowSecondLine.X = 1;
+            _rightTextFourRowSecondLine.Y = 1;
+
+            _rightTextFourRowThirdLine.X = 1;
+            _rightTextFourRowThirdLine.Y = 1;
+
+            _rightTextFourRowFourthLine.X = 1;
+            _rightTextFourRowFourthLine.Y = 1;
         }
 
         [JsonIgnore]
@@ -124,14 +124,87 @@ namespace HgSoftware.InsertCreator.Model
             get 
             {
                 if (Properties.Settings.Default.LogoOnLefthand)
-                    return _logoPositionLeft;
-                return _logoPositionRight;
+                    return _leftLogoPosition;
+                return _rightLogoPosition;
             }           
         }
 
-     
+        [JsonIgnore]
+        public PointF TextOneRowFirstLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextOneRowFirstLine;
+                return _rightTextOneRowFirstLine;
+            }
+        }
 
+        [JsonIgnore]
+        public PointF TextTwoRowFirstLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextTwoRowFirstLine;
+                return _rightTextTwoRowFirstLine;
+            }
+        }
 
+        [JsonIgnore]
+        public PointF TextTwoRowSecondLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextTwoRowSecondLine;
+                return _rightTextTwoRowSecondLine;
+            }
+        }
+
+        [JsonIgnore]
+        public PointF TextFourRowFirstLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextFourRowFirstLine;
+                return _rightTextFourRowFirstLine;
+            }
+        }
+
+        [JsonIgnore]
+        public PointF TextFourRowSecondLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextFourRowSecondLine;
+                return _rightTextFourRowSecondLine;
+            }
+        }
+
+        [JsonIgnore]
+        public PointF TextFourRowThirdLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextFourRowThirdLine;
+                return _rightTextFourRowThirdLine;
+            }
+        }
+
+        [JsonIgnore]
+        public PointF TextFourRowFourthLinePosition
+        {
+            get
+            {
+                if (Properties.Settings.Default.LogoOnLefthand)
+                    return _leftTextFourRowFourthLine;
+                return _rightTextFourRowFourthLine;
+            }
+        }
         #endregion Private Fields
     }
 }
